aa547c794710e9b60e0beb72fa470d23db8136c0
cnkumbula <klaudio.kumbula@gmail.com>
2024-03-06 12:10:51 +0200

    voteOnPoll

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,6 +1,22 @@
 import fastify from 'fastify'
+import cookie from '@fastify/cookie'
+import { createPool } from '../routes/createPoll'
+import { getPool } from '../routes/getPoll'
+import { voteOnPoll } from '../routes/voteOnPoll'
 
 const app = fastify()
+
+app.register(cookie,{
+  secret: "my-secret-key", // for cookies signature
+  hook: 'onRequest', // set to false to disable cookie autoparsing or set autoparsing on any of the following hooks: 'onRequest', 'preParsing', 'preHandler', 'preValidation'. default: 'onRequest'
+ // options for parsing cookies
+} )
+
+app.register(createPool)
+app.register(getPool)
+app.register(voteOnPoll)
+
+
 
 
 app.listen({ port: 3333 }).then(() => {
